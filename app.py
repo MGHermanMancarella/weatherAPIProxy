a7010f8f4a14ff428e1da275c99bd8df22713fd7
Michael <mherman315@gmail.com>
2023-09-29 11:02:10 -0700

    .env, app: Added openai key and route

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import requests
 from flask_cors import CORS
 
 WEATHER_KEY = os.environ['WEATHER_KEY']
+openai.api_key = os.environ['OPENAI_SECRET_KEY']
 
 app = Flask(__name__)
 CORS(app, resources={r"*": {"origins": "https://isitglizzyweather.site"}})
